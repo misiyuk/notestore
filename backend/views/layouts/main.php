b2bcba443036b4d6d90ababa28ee4ba9620b24c4
Misiyuk <6olinet@gmail.com>
2018-10-16 23:05:20 +0300

    Exist "main-login" layout in controller.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,32 +1,12 @@
 <?php
 use yii\helpers\Html;
-
 /* @var $this \yii\web\View */
 /* @var $content string */
-
-
-if (Yii::$app->controller->action->id === 'login') { 
-/**
- * Do not use this code in your template. Remove it. 
- * Instead, use the code  $this->layout = '//main-login'; in your controller.
- */
-    echo $this->render(
-        'main-login',
-        ['content' => $content]
-    );
-} else {
-
-    if (class_exists('backend\assets\AppAsset')) {
-        backend\assets\AppAsset::register($this);
-    } else {
-        app\assets\AppAsset::register($this);
-    }
-
-    dmstr\web\AdminLteAsset::register($this);
-
-    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
-    ?>
-    <?php $this->beginPage() ?>
+backend\assets\AppAsset::register($this);
+dmstr\web\AdminLteAsset::register($this);
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
+?>
+<?php $this->beginPage() ?>
     <!DOCTYPE html>
     <html lang="<?= Yii::$app->language ?>">
     <head>
@@ -61,5 +41,4 @@ if (Yii::$app->controller->action->id === 'login') {
     <?php $this->endBody() ?>
     </body>
     </html>
-    <?php $this->endPage() ?>
-<?php } ?>
+<?php $this->endPage() ?>
